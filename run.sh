80ed1550c6f4190d1d4b14b93e3e0e5d33e91a56
Tim Veil <tjveil@gmail.com>
2018-10-15 12:51:19 -0400

    moving back to root

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 echo "SIGN_URL_ENDPOINT=http://${STORAGE_HOSTNAME}:8080" >> /takipi-server/conf/tomcat-original/my.storage.properties
 
-/takipi-server/bin/takipi-server.sh --host-url ${SERVER_HOSTNAME} --db-url jdbc:mysql://db:${DB_PORT} --db-user overops --db-password overops -v start && tail --retry -f -n +1 /takipi-server/log/tomcat/tomcat/catalina.log
+/takipi-server/bin/takipi-server.sh --host-url ${SERVER_HOSTNAME} --db-url jdbc:mysql://db:${DB_PORT} --db-user root --db-password overops -v start && tail --retry -f -n +1 /takipi-server/log/tomcat/tomcat/catalina.log
