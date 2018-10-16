aaffb1b1226d4d5fce731b190509cbaeccd0544b
Tim Veil <tjveil@gmail.com>
2018-10-16 14:18:07 -0400

    trying to get console working

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 echo "SIGN_URL_ENDPOINT=http://${STORAGE_HOSTNAME}:8080" >> /takipi-server/conf/tomcat-original/my.storage.properties
 
-/takipi-server/bin/takipi-server.sh --host-url ${SERVER_HOSTNAME} -v start && tail --retry -f -n +1 /takipi-server/log/tomcat/tomcat/catalina.log && java -jar /takipi-server/lib/h2.jar
+/takipi-server/bin/takipi-server.sh --host-url ${SERVER_HOSTNAME} -v start && tail --retry -f -n +1 /takipi-server/log/tomcat/tomcat/catalina.log
