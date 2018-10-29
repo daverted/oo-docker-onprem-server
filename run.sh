5f99c003e0ed77e94888bdc4e7ddd565c2f301ec
Tim Veil <tjveil@gmail.com>
2018-10-29 10:57:29 -0400

    updating after fix

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(/takipi-server/bin/takipi-server.sh --host-url ${SERVER_HOSTNAME} -v start &) && (java -jar /takipi-server/lib/h2.jar -webAllowOthers &) && (tail --retry -f -n +1 /takipi-server/log/tomcat/tomcat/catalina.log)
+(/takipi-server/bin/takipi-server.sh --host-url ${HOST_URL} --frontend-url ${FRONTEND_URL} -v start &) && (java -jar /takipi-server/lib/h2.jar -webAllowOthers &) && (tail --retry -f -n +1 /takipi-server/log/tomcat/tomcat/catalina.log)
