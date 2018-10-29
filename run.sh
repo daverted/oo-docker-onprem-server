a785c8844cdc828e58a41e6604ed822fdc96de02
Tim Veil <tjveil@gmail.com>
2018-10-29 11:11:33 -0400

    updating after fix

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(/takipi-server/bin/takipi-server.sh -host-url ${HOST_URL} --frontend-url ${FRONTEND_URL} --db-url jdbc:mysql://db:${DB_PORT} --db-user root --db-password overops -v start &) && (tail --retry -f -n +1 /takipi-server/log/tomcat/tomcat/catalina.log)
+(/takipi-server/bin/takipi-server.sh --host-url ${HOST_URL} --frontend-url ${FRONTEND_URL} --db-url jdbc:mysql://db:${DB_PORT} --db-user root --db-password overops -v start &) && (tail --retry -f -n +1 /takipi-server/log/tomcat/tomcat/catalina.log)
